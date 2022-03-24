ec0f2f864d4227eb452d1bf70cedfd7bf9fe28af
Chris de la Garza <ckeithd@gmail.com>
2022-03-24 16:14:39 -0500

    first commit

---
+++ rulesets/main.tf
@@ -0,0 +1,25 @@
+# This file is sourced from the Example Usage at https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs
+# Version of Cloudflare provider is declared here along with additional providers needed for our config
+# Configure the Cloudflare provider using the required_providers stanza required with Terraform 0.13 and beyond
+# You may optionally use version directive to prevent breaking changes occurring unannounced.
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  email      = var.cloudflare_email
+  api_key    = var.cloudflare_api_key
+  account_id = var.cloudflare_account_id
+}
+
+#provider "google" {
+#  project    = var.gcp_project_id
+#}
+
+#provider "random" {
+#}
