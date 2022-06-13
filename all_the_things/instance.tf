81a26fe97781a1a9b81ac823fa46d16da1ac3df4
Chris de la Garza <ckeithd@gmail.com>
2022-06-13 17:27:24 -0500

    working ansible/terraform commit for instance

---
+++ all_the_things/instance.tf
@@ -0,0 +1,57 @@
+resource "random_id" "namespace" {
+  prefix      = "dlg-"
+  byte_length = 2
+}
+
+data "google_compute_image" "image" {
+  family  = "ubuntu-minimal-2004-lts"
+  project = "ubuntu-os-cloud"
+}
+
+resource "google_compute_instance" "origin" {
+  name         = random_id.namespace.hex
+  machine_type = var.machine_type
+  zone         = var.zone
+  tags         = ["http-server", "ssh", "https-server"]
+
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.image.self_link
+    }
+  }
+  
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  scheduling {
+    preemptible = true
+    automatic_restart = false
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update", "sudo apt install python3 -y",  "echo Done!"
+    ]
+    connection {
+      #host = google_compute_instance.origin.network_interface[0].access_config[0].nat_ip
+      host = self.network_interface.0.access_config.0.nat_ip
+      user = "dlg"
+      type = "ssh"
+    }
+  }
+
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook  -i ${self.network_interface.0.access_config.0.nat_ip}, scripts/main.yml"
+  }
+
+  metadata = {
+      cf-terraform = "demo_tf_kitchensink"
+      cf-email     = var.cloudflare_email
+      cf-zone      = var.cloudflare_zone
+  } 
+
+}
