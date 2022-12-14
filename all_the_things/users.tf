f67d79b6646c5547e6c3379bdf29e890e0c1af55
Chris de la Garza <ckeithd@gmail.com>
2022-12-14 16:42:45 -0600

    update with RBAC example

---
+++ all_the_things/users.tf
@@ -0,0 +1,20 @@
+data "cloudflare_account_roles" "rbac" {
+  account_id = var.cloudflare_account_id
+}
+
+locals {
+  roles_by_name = {
+    for role in data.cloudflare_account_roles.rbac.roles :
+      role.name => role
+  }
+}
+
+resource "cloudflare_account_member" "example_user" {
+  email_address = "user@example.com"
+  account_id = var.cloudflare_account_id
+  role_ids = [
+    local.roles_by_name["Firewall"].id,
+    local.roles_by_name["DNS"].id 
+  ]
+}
+
